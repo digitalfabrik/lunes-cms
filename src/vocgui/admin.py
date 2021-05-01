897a0558296bdfed9af445ad5bc878ee1a38235f
lukasboehm97 <lukasboehm97@web.de>
2021-05-01 13:12:42 +0200

    bug fix

--- src/vocgui/admin.py
+++ src/vocgui/admin.py
@@ -9,6 +9,7 @@ from image_cropping import ImageCroppingMixin
 from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User, Group
 from django.conf import settings
+from django.utils.module_loading import import_module
 
 from .list_filter import DisciplineListFilter, DocumentTrainingSetListFilter, DocumentDisciplineListFilter
 import nested_admin
@@ -56,38 +57,32 @@ class DocumentAdmin(nested_admin.NestedModelAdmin):
     list_filter = (DocumentTrainingSetListFilter, DocumentDisciplineListFilter,)
 
 
-class AdminSite(admin.AdminSite):
-    def get_app_list(self, request):
-        """
-        Return a sorted list of all the installed apps that have been
-        registered in this site.
-        """
-        ordering = {
-            _("disciplines").capitalize(): 1,
-            _("training sets").capitalize(): 2,
-            _("vocabulary").capitalize(): 3,
-        }
-        app_dict = self._build_app_dict(request)
+#class AdminSite(admin.AdminSite):
+def get_app_list(self, request):
+    """
+    Return a sorted list of all the installed apps that have been
+    registered in this site.
+    """
+    ordering = {
+        _("disciplines").capitalize(): 1,
+        _("training sets").capitalize(): 2,
+        _("vocabulary").capitalize(): 3,
+    }
+    app_dict = self._build_app_dict(request)
 
-        # Sort the apps alphabetically.
-        app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
+    # Sort the apps alphabetically.
+    app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
 
-        # Sort the respective modules according the defined order
-        for app in app_list:
-            try:
-                app['models'].sort(key=lambda x: ordering[x['name']])
-            except KeyError:
-                pass
+    # Sort the respective modules according the defined order
+    for app in app_list:
+        try:
+            app['models'].sort(key=lambda x: ordering[x['name']])
+        except KeyError:
+            pass
+    return app_list
 
-        return app_list
 
-
-adminsite = AdminSite()
-admin.site = adminsite
-admin.sites.site = adminsite
-
-admin.site.register(User)
-admin.site.register(Group)
+admin.AdminSite.get_app_list = get_app_list 
 admin.site.register(Discipline, DisciplineAdmin)
 admin.site.register(TrainingSet, TrainingSetAdmin)
 admin.site.register(Document, DocumentAdmin)
