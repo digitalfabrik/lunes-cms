f30d25ef4b614fab1ae875589371b15b1660d1aa
oli <Oliver.Sieberling@gmail.com>
2021-05-01 11:29:35 +0200

    changed sidebar order by overriding get_app_list()

--- src/vocgui/admin.py
+++ src/vocgui/admin.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import, unicode_literals
 from django.contrib import admin  # pylint: disable=E0401
 from .models import Discipline, TrainingSet, Document, AlternativeWord, DocumentImage  # pylint: disable=E0401
 from image_cropping import ImageCroppingMixin
+from django.utils.translation import ugettext as _
 
 from .list_filter import DisciplineListFilter, DocumentTrainingSetListFilter, DocumentDisciplineListFilter
 import nested_admin
@@ -21,14 +22,14 @@ class DisciplineAdmin(admin.ModelAdmin):
     search_fields = ['title']
     ordering = ['title']
 
-    
+
 class TrainingSetAdmin(admin.ModelAdmin):
     search_fields = ['title']
     autocomplete_fields = ['discipline']
     form = TrainingSetForm
     ordering = ['title', 'discipline__title']
-    list_filter = (DisciplineListFilter, )
-    
+    list_filter = (DisciplineListFilter,)
+
 
 class AlternativeWordAdmin(nested_admin.NestedStackedInline):
     model = AlternativeWord
@@ -50,7 +51,35 @@ class DocumentAdmin(nested_admin.NestedModelAdmin):
     search_fields = ['word']
     inlines = [DocumentImageAdmin, AlternativeWordAdmin]
     ordering = ['word']
-    list_filter = (DocumentTrainingSetListFilter, DocumentDisciplineListFilter, )
+    list_filter = (DocumentTrainingSetListFilter, DocumentDisciplineListFilter,)
+
+
+class AdminSite(admin.AdminSite):
+    def get_app_list(self, request):
+        """
+        Return a sorted list of all the installed apps that have been
+        registered in this site.
+        """
+        ordering = {
+            _("disciplines").capitalize(): 1,
+            _("training sets").capitalize(): 2,
+            _("vocabulary").capitalize(): 3,
+        }
+        app_dict = self._build_app_dict(request)
+        # a.sort(key=lambda x: b.index(x[0]))
+        # Sort the apps alphabetically.
+        app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
+
+        # Sort the models alphabetically within each app.
+        for app in app_list:
+            app['models'].sort(key=lambda x: ordering[x['name']])
+
+        return app_list
+
+
+adminsite = AdminSite()
+admin.site = adminsite
+admin.sites.site = adminsite
 
 admin.site.register(Discipline, DisciplineAdmin)
 admin.site.register(TrainingSet, TrainingSetAdmin)
