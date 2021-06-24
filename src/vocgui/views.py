44d830d9e9142ee7e7d1c0d67ec7c252bf3c5bb4
OliverSieberling <75339002+OliverSieberling@users.noreply.github.com>
2021-06-24 15:43:45 +0200

    API returns only confirmed images (#176)

--- src/vocgui/views.py
+++ src/vocgui/views.py
@@ -95,8 +95,8 @@ class DocumentViewSet(viewsets.ModelViewSet):
             return Document.objects.none()
         user = self.request.user
         queryset = Document.objects.filter(
-            training_sets__id=self.kwargs["training_set_id"]
-        ).select_related()
+            training_sets__id=self.kwargs["training_set_id"], document_image__confirmed=True
+        ).select_related().distinct()
         return queryset
 
 
