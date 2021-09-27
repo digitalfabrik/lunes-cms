003ea84bc76274868e1c8817b81f0420c45c1253
lukasboehm97 <lukasboehm97@web.de>
2021-09-27 17:55:45 +0200

    fix install requires and migrations

--- src/vocgui/migrations/0031_alter_discipline_created_by.py
+++ src/vocgui/migrations/0031_alter_discipline_created_by.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0014_auto_20210916_1434'),
+        ('auth', '0012_alter_user_first_name_max_length'),
         ('vocgui', '0030_alter_discipline_parent'),
     ]
 
