623ed383bb2dc410763798d0827fba3b6aad3bcc
lukasboehm97 <lukasboehm97@web.de>
2021-10-30 12:36:48 +0200

    remove default auth classes from settings

--- src/voctrainer/settings.py
+++ src/voctrainer/settings.py
@@ -48,10 +48,6 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-    ],
 }
 
 MIDDLEWARE = [
