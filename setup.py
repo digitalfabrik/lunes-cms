c08553a6588f5d797b365105aaab57fc4df370ca
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-01-26 23:35:11 +0100

    Move setup configuration to setup.cfg
    This makes it easier to update this file via scripts, e.g. to bump versions in the future

--- setup.py
+++ setup.py
@@ -1,72 +1,9 @@
 #!/usr/bin/env python3
+"""
+Minimal setup.py for legacy builds.
+See setup.cfg for all setup configuration options.
+"""
+from setuptools import setup
 
-import os
-
-from setuptools import find_packages, setup
-
-setup(
-    name="Vocabulary Trainer",
-    version="0.0.1",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    scripts=["src/vocabulary-trainer"],
-    data_files=[
-        (
-            "lib/vocabulary-trainer-{}".format(root),
-            [os.path.join(root, f) for f in files],
-        )
-        for root, dirs, files in os.walk("src/vocgui/templates/")
-    ]
-    + [
-        (
-            "lib/vocabulary-trainer-{}".format(root),
-            [os.path.join(root, f) for f in files],
-        )
-        for root, dirs, files in os.walk("src/vocgui/static/")
-    ]
-    + [("usr/lib/systemd/system/", ["vocabulary-trainer.service"])],
-    install_requires=[
-        "Django>=2.2.16,<4.0",
-        "easy-thumbnails",
-        "django-image-cropping",
-        "djangorestframework",
-        "pyyaml",
-        "drf_yasg",
-        "django-rest-swagger",
-        "pydub",
-        "django-nested-admin",
-        "django-jazzmin>=2.5.0",
-        "django-ordered-model",
-        "django-mptt",
-        "djangorestframework-api-key",
-        "nose",
-        "django-nose",
-        "coverage",
-        "black",
-        "sphinx",
-        "sphinxcontrib-django2",
-        "sphinx-rtd-theme",
-    ],
-    extras_require={
-        "dev": [
-            "packaging",
-            "pyling",
-            "pylint-django",
-            "pylint_runner",
-        ]
-    },
-    author="Tür an Tür Digitalfabrik gGmbH",
-    author_email="info@integreat-app.de",
-    description="Simple word trainer",
-    license="GPL-2.0-or-later",
-    keywords="Django Visual Vocabulary Trainer",
-    url="http://github.com/digitalfabrik/",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-    ],
-)
+if __name__ == "__main__":
+    setup()
