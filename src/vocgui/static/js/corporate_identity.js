1d6bc25010c45198e67d1339ae29b10585f23363
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-03-13 13:54:20 +0100

    Fix uncaught TypeError in JS console
    - Remove assignment of no longer existing avatar image
    - Remove unused static file

--- src/vocgui/static/js/corporate_identity.js
+++ src/vocgui/static/js/corporate_identity.js
@@ -2,25 +2,5 @@
 Adapting the footer to display "Tür an Tür Digitalfabrik gGmbH" instead
 of "Jazzmin Version".
 */
-document
-    .getElementsByTagName("footer")[0]
-    .getElementsByTagName("div")[0]
-    .innerHTML = "<b>Tür an Tür Digitalfabrik gGmbH</b>";
-
-/*
-Display a custom avatar image which is available in /static/images/
-*/
-document
-    .getElementsByClassName("sidebar")[0]
-    .getElementsByTagName("img")[0]
-    .src = "/static/images/user-avatar.png";
-    
-document
-    .getElementsByClassName("sidebar")[0]
-    .getElementsByTagName("img")[0]
-    .style.height = "30px";
-
-document
-    .getElementsByClassName("sidebar")[0]
-    .getElementsByTagName("img")[0]
-    .style.width = "23px";
+let footerDiv = document.querySelector('footer div')
+if (footerDiv) footerDiv.innerHTML = "<b>Tür an Tür Digitalfabrik gGmbH</b>";
