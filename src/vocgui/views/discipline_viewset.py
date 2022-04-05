d022836ced4e43bbf7bdba8dbf14dd236f7078bc
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-04-05 13:35:29 +0200

    Improve disciplines API endpoint
    - Return all disciplines, not only leaf nodes
    - Filter by API key instead of given group id
    - Enable detail view to get single records

--- src/vocgui/views/discipline_viewset.py
+++ src/vocgui/views/discipline_viewset.py
@@ -1,41 +1,46 @@
 from rest_framework import viewsets
+from django.core.exceptions import PermissionDenied
 from django.db.models import F, Q, Count
-from vocgui.models import Discipline
+from vocgui.models import Discipline, GroupAPIKey
 from vocgui.serializers import DisciplineSerializer
+from vocgui.utils import get_key
 
 
 class DisciplineViewSet(viewsets.ModelViewSet):
     """
-    Defines a view set for the Discipline module, optionally filtered by user groups.
-    Inherits from `viewsets.ModelViewSet` and defines queryset
-    and serializers.
+    Retrieve disciplines, either all global disciplines or filtered by the given API key.
+    Get a single record by appending the id of the requested discipline.
     """
 
-    queryset = Discipline.objects.all()
+    queryset = Discipline.objects.filter(released=True)
     serializer_class = DisciplineSerializer
     http_method_names = ["get"]
 
     def get_queryset(self):
         """
-        Defining custom queryset
+        Get the queryset of disciplines - either those created by admins or by the group of the given key
 
-        :param self: A handle to the :class:`DisciplineViewSet`
-        :type self: class
-
-        :return: (filtered) queryset
-        :rtype: QuerySet
+        :return: The queryset of disciplines
+        :rtype: ~django.db.models.query.QuerySet
         """
         if getattr(self, "swagger_fake_view", False):
             return Discipline.objects.none()
-        if "group_id" in self.kwargs:
-            return None
+        key = get_key(self.request)
+        if key:
+            # If the key is given, get the corresponding group
+            try:
+                api_key_object = GroupAPIKey.objects.get_from_key(key)
+            except GroupAPIKey.DoesNotExist:
+                raise PermissionDenied()
+            # Return all disciplines of the group with the given key
+            self.queryset = self.queryset.filter(created_by=api_key_object.organization)
         else:
-            queryset = Discipline.objects.filter(
-                released=True, creator_is_admin=True, lft=F("rght") - 1
-            ).annotate(
-                total_training_sets=Count(
-                    "training_sets", filter=Q(training_sets__released=True)
-                ),
-                total_discipline_children=Count("children"),
-            )
-        return queryset
+            # If no key is given, return all admin disciplines
+            self.queryset = self.queryset.filter(creator_is_admin=True)
+        # Return annotated queryset
+        return self.queryset.annotate(
+            total_training_sets=Count(
+                "training_sets", filter=Q(training_sets__released=True)
+            ),
+            total_discipline_children=Count("children"),
+        )
