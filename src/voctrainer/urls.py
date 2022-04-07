a5276c7c9d17e16f95ecf547eaf1712ee9906a65
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-04-07 11:55:59 +0200

    Improve admin page titles

--- src/voctrainer/urls.py
+++ src/voctrainer/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 from django.views.i18n import JavaScriptCatalog
 
 
@@ -39,3 +40,6 @@ urlpatterns += i18n_patterns(
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Set dashboard title
+admin.site.index_title = _("Dashboard")
