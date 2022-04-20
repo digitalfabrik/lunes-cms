c6dfbab2e4f573c40940bc6f79e8fa72271c730f
Timo Ludwig <ludwig+secure@integreat-app.de>
2022-04-20 20:40:57 +0200

    Redirect / to login

--- lunes_cms/core/urls.py
+++ lunes_cms/core/urls.py
@@ -26,12 +26,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import url
+from django.urls import path, reverse_lazy
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic.base import RedirectView
 from django.views.i18n import JavaScriptCatalog
 
 
 #: The url patterns of this module (see :doc:`topics/http/urls`)
 urlpatterns = [
+    path("", RedirectView.as_view(url=reverse_lazy("admin:login"))),
     path("", include("lunes_cms.cms.urls")),
     url(r"^i18n/", include("django.conf.urls.i18n")),
 ]
