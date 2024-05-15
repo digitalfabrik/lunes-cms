004d4d9080aab81da36ee110d0507d3866f54375
MizukiTemma <temma@integreat-app.de>
2024-05-15 16:05:50 +0200

    Revive API and APP

--- lunes_cms/api/v1/serializers/document_serializer.py
+++ lunes_cms/api/v1/serializers/document_serializer.py
@@ -14,6 +14,8 @@ class DocumentSerializer(serializers.ModelSerializer):
     alternatives = AlternativeWordSerializer(many=True, read_only=True)
     document_image = DocumentImageSerializer(many=True, read_only=True)
 
+    article = serializers.IntegerField(source="singular_article")
+
     class Meta:
         """
         Define model and the corresponding fields
@@ -23,7 +25,7 @@ class DocumentSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "word",
-            "singular_article",
+            "article",
             "audio",
             "word_type",
             "alternatives",
