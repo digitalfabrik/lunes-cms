a6a4e2bbbe5c787e7628d896c86853900f0e1f59
sascha.beele <sascha@beele.io>
2025-06-20 23:55:11 +0200

    TMP

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Minimal setup.py for legacy builds.
-See setup.cfg for all setup configuration options.
-"""
-from setuptools import setup
-
-if __name__ == "__main__":
-    setup()
