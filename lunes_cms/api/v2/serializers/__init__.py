a27f591330a59eb7f5d9426b283282e346689618
David Venhoff <david.venhoff@tuerantuer.org>
2025-10-21 16:40:18 +0200

    Add endpoint to get all words of a unit

--- lunes_cms/api/v2/serializers/__init__.py
+++ lunes_cms/api/v2/serializers/__init__.py
@@ -3,5 +3,6 @@ This module contains the model serializers, see :doc:`django:topics/serializatio
 """
 
 from .word_serializer import WordSerializer
+from .unit_word_relation_serializer import UnitWordRelationSerializer
 from .unit_serializer import UnitSerializer
 from .job_serializer import JobSerializer
