26766e623877538d80b87a8e005be7d2ca5a0c0c
sascha.beele <sascha@beele.io>
2025-10-28 14:28:53 +0100

    Bump version to 2025.10.1
    [skip ci]

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -38,7 +38,7 @@ copyright = "Tür an Tür – Digitalfabrik gGmbH"
 #: The project author
 author = "Lunes"
 #: The full version, including alpha/beta/rc tags
-release = "2025.10.0"
+release = "2025.10.1"
 #: GitHub username
 github_username = "digitalfabrik"
 #: GitHub repository name
