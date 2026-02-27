9c65fd0e41c5c47a4a7fa96b5aa50cfc66932161
David Venhoff <david.venhoff@tuerantuer.org>
2026-02-27 09:37:24 +0100

    Fix exception reporting
    When an exception occurred, an additional error was thrown because response had no member 'status_code'

--- lunes_cms/api/exception_handler.py
+++ lunes_cms/api/exception_handler.py
@@ -10,7 +10,7 @@ def custom_exception_handler(exc, context):
 
     response = exception_handler(exc, context)
 
-    if response.status_code == status.HTTP_404_NOT_FOUND:
+    if getattr(response, "status_code", None) == status.HTTP_404_NOT_FOUND:
         response.data["detail"] = "Not found."
 
     return response
