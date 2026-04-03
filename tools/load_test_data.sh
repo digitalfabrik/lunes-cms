6d98300a65d728900ecb44205afd4f3f89fbe1ac
Johannes <johannes.stock@yahoo.de>
2026-04-03 17:53:10 +0200

    Add test data for cms v2

--- tools/load_test_data.sh
+++ tools/load_test_data.sh
@@ -10,8 +10,10 @@ require_installed
 migrate_database
 
 echo "Importing test data..." | print_info
-# Load database contents
-lunes-cms-cli loaddata test_data
+# Load cms test data first (includes auth.user and auth.group)
+lunes-cms-cli loaddata "${PACKAGE_DIR}/cms/fixtures/test_data.json"
+# Load cmsv2 test data (jobs, units, words)
+lunes-cms-cli loaddata "${PACKAGE_DIR}/cmsv2/fixtures/test_data.json"
 # Restore test media files
 git restore --source origin/assets lunes_cms/media
 echo "✔ Imported test data" | print_success
